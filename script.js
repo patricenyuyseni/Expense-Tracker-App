d3b16aa8a8b638abbae69d2f0625d316f6f7598b
Nyuyseni Patrice <patricenyuyseni>
2026-03-13 13:42:12 +0100

    changes made on both the script.js file and style.css file

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const expenseListEl = document.getElementById("expense-list");
 
 let balance = 0;
 addExpenseBtn.addEventListener("click", () =>{
-    const name = addexpenseNameEl.value.trim();
+    const name = expenseNameEl.value.trim();
     const amount = parseFloat(expenseAmountEl.value);
 
     if(name ==="" || isNaN(amount) ||amount <= 0){
@@ -17,11 +17,11 @@ addExpenseBtn.addEventListener("click", () =>{
     balanceEl.textContent = balance.toFixed(2);
 
     const li = document.createElement('li');
-    li.innerHTML = ${name} - $${amount.toFixed(2)}
-    <button class= "delete-btn">x</button>
+    li.innerHTML = `${name} - $${amount.toFixed(2)}
+    <button class= "delete-btn">x</button>`
     expenseListEl.appendChild(li);
 
-    li.querySelector("delete-btn").addEventListener("click", () =>{
+    li.querySelector(".delete-btn").addEventListener("click", () =>{
         balance += amount;
         balanceEl.textContent= balance.toFixed(2);
         li.remove();
