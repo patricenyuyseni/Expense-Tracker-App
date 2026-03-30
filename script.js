b6e47ba962b1027133a154fdda9fe198f9b59e79
Nyuyseni Patrice <patricenyuyseni>
2026-03-30 14:20:37 +0100

    changes made on the whole expense-tracker app

--- script.js
+++ script.js
@@ -1,34 +1,87 @@
-const balanceEl = document.getElementById("balance");
-const expenseNameEl = document.getElementById("expense-name");
-const addExpenseBtn = document.getElementById("add-expense-btn");
-const expenseAmountEl = document.getElementById("expense-amount");
-const expenseListEl = document.getElementById("expense-list");
+const balance = document.getElementById('balance');
+const income = document.getElementById('income');
+const expense = document.getElementById('expense');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
 
-let balance = 0;
-addExpenseBtn.addEventListener("click", () =>{
-    const name = expenseNameEl.value.trim();
-    const amount = parseFloat(expenseAmountEl.value);
+// Get from localStorage
+let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
 
-    if(name ==="" || isNaN(amount) ||amount <= 0){
-        alert("please enter a valid expense name and amount");
-        return;
-    }
-    balance -= amount 
-    balanceEl.textContent = balance.toFixed(2);
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
 
-    const li = document.createElement('li');
-    li.innerHTML = `${name} - $${amount.toFixed(2)}
-    <button class= "delete-btn">x</button>`
-    expenseListEl.appendChild(li);
+  const transaction = {
+    id: Date.now(),
+    text: text.value,
+    amount: +amount.value
+  };
 
-    li.querySelector(".delete-btn").addEventListener("click", () =>{
-        balance += amount;
-        balanceEl.textContent= balance.toFixed(2);
-        li.remove();
-    })
-    expenseNameEl.value= "";
-    expenseAmountEl.value= "";
+  transactions.push(transaction);
+  updateLocalStorage();
+  init();
 
-} 
+  text.value = '';
+  amount.value = '';
+}
 
-)
+// Display transaction
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const item = document.createElement('li');
+
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} 
+    <span>${sign}$${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+// Update totals
+function updateValues() {
+  const amounts = transactions.map(t => t.amount);
+
+  const total = amounts.reduce((acc, val) => acc + val, 0).toFixed(2);
+  const inc = amounts
+    .filter(val => val > 0)
+    .reduce((acc, val) => acc + val, 0)
+    .toFixed(2);
+
+  const exp = (
+    amounts
+      .filter(val => val < 0)
+      .reduce((acc, val) => acc + val, 0) * -1
+  ).toFixed(2);
+
+  balance.textContent = `$${total}`;
+  income.textContent = `$${inc}`;
+  expense.textContent = `$${exp}`;
+}
+
+
+function removeTransaction(id) {
+  transactions = transactions.filter(t => t.id !== id);
+  updateLocalStorage();
+  init();
+}
+
+
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+
+function init() {
+  list.innerHTML = '';
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+form.addEventListener('submit', addTransaction);
